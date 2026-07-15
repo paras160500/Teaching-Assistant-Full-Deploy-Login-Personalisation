0c25c509aac1e22d77586cc31ff58940370eef0b
Paras Patel <paraspatel160500@gmail.com>
2026-07-15 11:27:17 +0530

    mongo health check

--- server/main.py
+++ server/main.py
@@ -5,6 +5,9 @@ from fastapi import FastAPI
 from auth.route import router as auth_router
 from docs.route import router as doc_router
 from chat.route import router as chat_router
+from pymongo import MongoClient
+import os
+
 
 #-----------------------------------------------------------------------
 #                           Logic Statements
@@ -17,3 +20,12 @@ app.include_router(chat_router)
 @app.get("/")
 def home():
     return {"message" : "Welcome to the user Management API"}
+
+@app.get("/mongo-test")
+def mongo_test():
+    try:
+        client = MongoClient(os.getenv("MONGO_URI"))
+        client.admin.command("ping")
+        return {"status": "Connected to MongoDB"}
+    except Exception as e:
+        return {"status": "Failed", "error": str(e)}
