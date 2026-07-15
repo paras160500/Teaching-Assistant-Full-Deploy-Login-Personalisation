2ef70155effa390f23ee2dcc2af296b4af0da7d3
Paras Patel <paraspatel160500@gmail.com>
2026-07-15 10:28:04 +0530

    Quiz module added

--- server/chat/chat_query.py
+++ server/chat/chat_query.py
@@ -34,7 +34,7 @@ embed_model = OpenAIEmbeddings(
 # Define model
 llm = ChatGroq(model = "llama-3.3-70b-versatile" , temperature=0.3 , api_key=GROQ_API_KEY)
 
-# Define chat prompt 
+# Define chat prompt for RAG 
 rag_prompt=PromptTemplate.from_template(
     """
 You are a helpful educational assistant.
@@ -51,8 +51,31 @@ If relevant, mention the document source.
 """
 )
 
+# Define Chat Prompt for Quiz Generation
+quiz_prompt=PromptTemplate.from_template(
+     """
+You are a test-generating assistant.
+
+Using the context below, generate {num_questions}
+multiple-choice questions.
+
+Format STRICTLY as:
+
+Question 1: ...
+A) ...
+B) ...
+C) ...
+Correct Answer: A
+
+Context:
+{context}
+"""
+)
+
 # Define the RAG Chain
 rag_chain = rag_prompt | llm 
+# Define the quiz chain 
+quiz_chain = quiz_prompt | llm
 
 # Define the chat function
 async def answer_query(query : str , user_role : str , user_grade : int) -> dict:
@@ -65,7 +88,9 @@ async def answer_query(query : str , user_role : str , user_grade : int) -> dict
             user_role(str) : Role of user 
             user_grade(int) : Mostly grade of student
         Returns:
-            return a dict having two key as answer and sources
+            dict: A dictionary containing two keys:
+                - answer: The generated answer or an appropriate error message.
+                - sources: A list of source documents used to generate the answer.
     """
     # Call embedding model to generate query embedding
     embedding = await asyncio.to_thread(embed_model.embed_query , query)
@@ -100,5 +125,58 @@ async def answer_query(query : str , user_role : str , user_grade : int) -> dict
 
     return {
         "answer" : answer_text,
+        "sources" : sources
+    }
+
+
+
+async def quiz_query(topic : str , user_role : str , user_grade : int , num_questions : int = 5) -> dict:
+    """
+        Will fetch relevant documents from Pinecone based on the given topic, retrieve
+        their corresponding chunks from MongoDB, combine them as context, and pass the
+        context to the LLM to generate a quiz.
+        Args:
+            topic (str): Topic for which the quiz should be generated.
+            user_role (str): Role of the user.
+            user_grade (int): Grade of the student.
+            num_questions (int): Number of quiz questions to generate. Defaults to 5.
+        Returns:
+            dict: A dictionary containing two keys:
+                - quiz: The generated quiz or an appropriate error message.
+                - sources: A list of source documents used to generate the quiz.
+    """
+    # Call embedding model to generate query embedding
+    embedding = await asyncio.to_thread(embed_model.embed_query , topic)
+    # Retrieve relevant embeddings from the pinecone
+    results = await asyncio.to_thread(
+        index.query , vector = embedding , top_k = 5,include_metadata = True,filter={
+            "grade":user_grade,
+            "role":{"$in":["Public",user_role]}
+        }
+    )
+    # Validation check
+    if not results.get("matches"):
+        return {"quiz" : "No relevent information found to generate quiz" , "sources" : []}
+    
+    # Getting context from mongo 
+    chunk_ids = [m['id'] for m in results['matches']]                       # Getting the chunk id
+    docs = list(chunk_collection.find({"chunk_id":{"$in":chunk_ids}}))      # Getting the docs
+    if not docs:                                                            # Simple Validation
+        return {"quiz":"Context unavailable for quiz","sources":[]}
+    doc_map = {d['chunk_id']:d for d in docs}                               # Maping the doc with id                   
+    ordered_map = [doc_map[cid] for cid in chunk_ids if cid in doc_map]     # Ordering the doc
+    context = "\n\n".join(d['text'] for d in ordered_map)                   # Generating the context
+    sources=list({ d["source"] for d in ordered_map})                       # Getting Sources
+    response = await asyncio.to_thread(                                     # Getting result from rag chain
+        quiz_chain.invoke , {"num_questions" : num_questions , "context" : context}
+    )
+    quiz_text=(                                                             # Getting content from the result
+        response.content                                                    # If not content then response in string
+        if hasattr(response,"content")
+        else str(response)
+    )
+
+    return {
+        "quiz" : quiz_text,
         "sources" : sources
     }
--- server/chat/route.py
+++ server/chat/route.py
@@ -3,11 +3,11 @@
 #-----------------------------------------------------------------------
 from fastapi import APIRouter,HTTPException,Depends,Body
 from auth.route import authenticate
-from chat.chat_query import answer_query
+from chat.chat_query import answer_query,quiz_query
 from pydantic import BaseModel
 from typing import List,Optional
 import datetime
-from config.db import chat_history_collection
+from config.db import chat_history_collection, quizzes_collection, quiz_history
 from bson.objectid import ObjectId
 
 #-----------------------------------------------------------------------
@@ -16,10 +16,25 @@ from bson.objectid import ObjectId
 
 router = APIRouter()
 
+# Quiz Model using Pydantic
+class QuizRequest(BaseModel):
+    topic : str 
+    num_questions : Optional[int] = 5
+
+class QuizAnswerRequest(BaseModel):
+    quiz_id : str 
+    answers : list[str]
+
 @router.post("/chat")
 async def chat(user=Depends(authenticate) , query:str = Body(... , embed=True)):
     """
-    
+        Handle a student's chat request by validating access, generating an AI response,
+        and storing the conversation in the chat history.
+        Args:
+            user (dict): Authenticated user details.
+            query (str): Question asked by the student.
+        Returns:
+            dict: A dictionary containing the generated answer and its sources.
     """
     # Validate if the user is student or not
     if user['role'] != "Student":
@@ -28,10 +43,6 @@ async def chat(user=Depends(authenticate) , query:str = Body(... , embed=True)):
     response = await answer_query(
         query, user['role'],user['grade']
     )
-    print("--"*40)
-    print(user)
-    print("*"*50)
-    print(response)
 
     chat_history_collection.insert_one({
         "user_id" : user['user_id'],
@@ -42,3 +53,144 @@ async def chat(user=Depends(authenticate) , query:str = Body(... , embed=True)):
     })
 
     return response
+
+
+@router.post("/quiz")
+async def quiz(request : QuizRequest , user=Depends(authenticate)):
+    """
+        Handle a student's quiz generation request, create a quiz based on the given
+        topic, store the generated quiz in the database, and return the quiz along
+        with its sources and quiz ID.
+        Args:
+            request (QuizRequest): Quiz generation request containing the topic and
+                number of questions.
+            user (dict): Authenticated user details.
+        Returns:
+            dict: A dictionary containing the generated quiz, its sources, and the
+            generated quiz ID.
+    """
+    # Validate if the user is student or not
+    if user['role'] != "Student":
+        raise HTTPException(status_code=403 , detail="Only student can generate quizes")
+    
+    # Getting response from the quiz_query
+    response = await quiz_query(
+        request.topic , 
+        user["role"],
+        user["grade"],
+        request.num_questions
+    )
+
+    # Generating quiz doc to push in the db
+    quiz_doc = {
+        "user_id" : user['user_id'],
+        "timestamp" : datetime.datetime.utcnow(),
+        "topic" : request.topic,
+        "quiz_data" : response['quiz'],
+        "sources" : response['sources']
+    }
+
+    # Pushing the doc in db
+    result = quizzes_collection.insert_one(quiz_doc)
+
+    # Return quiz sources and quiz id to user 
+    return {
+        "quiz" : response['quiz'],
+        "sources" : response['sources'],
+        "quiz_id" : str(result.inserted_id)
+    }
+
+
+@router.post("/quiz/check")
+async def chec_quiz_answers(request : QuizAnswerRequest , user=Depends(authenticate)):
+    """
+        Check a student's quiz answers, calculate the score, save the quiz result
+        in the database, and return the score with answer details.
+        Args:
+            request (QuizAnswerRequest): Quiz answer submission containing the quiz
+                ID and selected answers.
+            user (dict): Authenticated user details.
+        Returns:
+            dict: A dictionary containing the quiz score, total questions, answer
+            results, and a completion message.
+    """
+    # Find the quiz id
+    quiz_doc = quizzes_collection.find_one(
+        {"_id" : ObjectId(request.quiz_id)}
+    )
+
+    # What if quiz not found
+    if not quiz_doc:
+        raise HTTPException(status_code=404 , detail="Quiz not found")
+    
+    # Check user id with quiz generated user id(by which user quiz generated)
+    if quiz_doc['user_id'] != user['user_id']:
+        raise HTTPException(status_code=403 , detail="UnAuthorize access")
+    
+    # Finding the answer
+    correct_answers = []
+    for line in quiz_doc['quiz_data'].split("\n"):
+        if line.startswith("Correct Answer:"):
+            correct_answers.append(line.split(":")[1].strip()[0])
+
+    # Validate answers
+    if len(request.answers) != len(correct_answers):
+        raise HTTPException(status_code=400 , detail="Answer count mismatch")
+    
+    score = 0
+    results = []
+
+    for i,ans in enumerate(request.answers):
+        is_correct = ans.strip().upper() == correct_answers[i]
+        if is_correct:
+            score +=1 
+
+        results.append({
+            "question_number" : i+1 ,
+            "user_answer" : ans,
+            "correct_answer" : correct_answers[i],
+            "is_correct" : is_correct
+        })
+
+    quiz_history.insert_one({
+        "user_id" : user['user_id'],
+        "quiz_id" : request.quiz_id,
+        "timestamp" : datetime.datetime.utcnow(),
+        "topic" : quiz_doc['topic'],
+        "score" : score ,
+        "total" : len(correct_answers),
+        "results" : results ,
+        "quiz_content" : quiz_doc['quiz_data']
+    })
+
+    return {
+        "message" : f"Quiz Completed, You scored : {score}/{len(correct_answers)}",
+        "score" : score,
+        "total" : len(correct_answers),
+        "results" : results
+    }
+
+
+@router.get("/quiz/history")
+async def get_quiz_history(user=Depends(authenticate)):
+    """
+    
+    """
+    # check the role of use is student or not
+    if user['role'] != "Student":
+        raise HTTPException(status_code=403 , detail="Only student can view the history")
+    
+    # fetch all the quiz history of particular user in reverse 
+    cursor = quiz_history.find({"user_id" : user['user_id']}).sort("timestamp" , -1)
+
+    history = []
+    for doc in cursor:
+        doc['id'] = str(doc.pop("_id"))
+        doc['quiz_id'] = str(doc['quiz_id'])
+        doc.pop("user_id" , None)
+        history.append(doc)
+
+    return {
+        "message" : f"Found {len(history)} attempts",
+        "history" : history
+    }
