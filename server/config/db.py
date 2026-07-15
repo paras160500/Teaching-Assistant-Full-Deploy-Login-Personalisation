dd4a2665121d4777d4f5011ca7b30345f54f2ca7
Paras Patel <paraspatel160500@gmail.com>
2026-07-15 11:39:33 +0530

    requirement added

--- server/config/db.py
+++ server/config/db.py
@@ -5,6 +5,8 @@
 import os 
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import certifi
+
 load_dotenv()
 
 
@@ -12,20 +14,36 @@ load_dotenv()
 #                           Logic Statements
 #-----------------------------------------------------------------------
 
-
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "test_db")
 
-client=MongoClient(MONGO_URI)
-db=client[DB_NAME]
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=10000
+)
+
+# Test connection (temporary)
+try:
+    client.admin.command("ping")
+    print("MongoDB connected successfully")
+except Exception as e:
+    print("MongoDB connection failed:", e)
+
+db = client[DB_NAME]
 
 # User collection
 users_collection = db["users"]
+
 # Document collection
-chunk_collection = db['text']
+chunk_collection = db["text"]
+
 # Chat collection
-chat_history_collection = db['chat_history']
+chat_history_collection = db["chat_history"]
+
 # Quiz collection
-quizzes_collection = db['quizzes']
-#Quiz history
-quiz_history = db['history']
+quizzes_collection = db["quizzes"]
+
+# Quiz history
+quiz_history = db["history"]
